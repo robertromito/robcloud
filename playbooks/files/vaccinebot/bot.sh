1d401d7b8c201c287f517ed63bcfeeb296728960
Rob Romito <rob@robertromito.com>
2021-02-15 16:34:21 -0500

    Adding playbook and cron

--- playbooks/files/vaccinebot/bot.sh
+++ playbooks/files/vaccinebot/bot.sh
@@ -2,23 +2,19 @@
 postTo='https://chat.home/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token='
 aa_token='%22UAQDrX1M46xDjfCeZWDbAr47Y0PiLqVsO3aE3zC06PyeO8vzbZowEgqglhN1Sql7%22'
 nac_token='%22hfaPmNSR9Hlq0THisjmmsOfU66FxCus3NDcgKt7K6oGp60PK1moq5jnNRg1NRimL%22'
-while true; do
 
-    results=$(http https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers | jq '.providerList[] | select(.address == "Buffalo, NY")')
-    echo "$(date) - ${results}"
-    
-    if $(echo $results | grep "AA"); then
-        msg="$(date) There are appointments available"
-        token=$aa_token
-    else
-        msg="$(date) There are no appoinments available"
-        token=$nac_token
-    fi
+results=$(http https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers | jq '.providerList[] | select(.address == "Buffalo, NY")')
+echo "$(date) - ${results}"
 
-    http \
-        --print=b --verify=no \
-        -f POST "https://chat.home/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=${token}" \
-        payload="{\"text\": \"${msg}\"}"
+if $(echo $results | grep "AA"); then
+    msg="$(date) There are appointments available"
+    token=$aa_token
+else
+    msg="$(date) There are no appoinments available"
+    token=$nac_token
+fi
 
-    sleep 30
-done
+http \
+    --print=b --verify=no \
+    -f POST "https://chat.home/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=${token}" \
+    payload="{\"text\": \"${msg}\"}"
