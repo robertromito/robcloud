985325fde8ff42e89c280787f56e0e50de1c7182
Rob Romito <rob@robertromito.com>
2021-02-15 17:28:49 -0500

    More tweaks

--- playbooks/files/vaccinebot/bot.sh
+++ playbooks/files/vaccinebot/bot.sh
@@ -3,7 +3,7 @@ postTo='https://chat.home/webapi/entry.cgi?api=SYNO.Chat.External&method=incomin
 aa_token='%22UAQDrX1M46xDjfCeZWDbAr47Y0PiLqVsO3aE3zC06PyeO8vzbZowEgqglhN1Sql7%22'
 nac_token='%22hfaPmNSR9Hlq0THisjmmsOfU66FxCus3NDcgKt7K6oGp60PK1moq5jnNRg1NRimL%22'
 
-results=$(http https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers | jq '.providerList[] | select(.address == "Buffalo, NY")')
+results=$(http GET https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers | jq '.providerList[] | select(.address == "Buffalo, NY")')
 echo "$(date) - ${results}"
 
 if $(echo $results | grep "AA"); then
@@ -14,7 +14,6 @@ else
     token=$nac_token
 fi
 
-http \
-    --print=b --verify=no \
-    -f POST "https://chat.home/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=${token}" \
+http --ignore-stdin --verify=no -f POST \
+    "https://chat.home/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=${token}" \
     payload="{\"text\": \"${msg}\"}"
